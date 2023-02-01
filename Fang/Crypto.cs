e9807582a74554d4886d2f02d98dfd1a07855272
Naomi Ahmed <yretenai@gmail.com>
2023-02-01 17:35:36 +0000

    Code style

--- Fang/Crypto.cs
+++ Fang/Crypto.cs
@@ -6,7 +6,7 @@ namespace Fang;
 
 public static class Crypto {
     public static ulong CalculateFilelistSeed(ulong a, ulong b) => CalculateFilelistSeed(MemoryMarshal.AsBytes(new[] { a, b }.AsSpan()));
-    public static ulong CalculateFilelistSeed(Span<byte> header) => (ulong)((header[9] << 24) | (header[12] << 16) | (header[2] << 8) | header[0]);
+    public static ulong CalculateFilelistSeed(Span<byte> header) => (ulong) ((header[9] << 24) | (header[12] << 16) | (header[2] << 8) | header[0]);
 
     // 009FCC10
     public static Span<byte> ExpandKey(ulong seed) {
@@ -15,8 +15,8 @@ public static class Crypto {
         var key64 = MemoryMarshal.Cast<byte, ulong>(key);
 
         // init seed vector
-        var loSeed = (uint)(seed & 0xFFFFFFFF);
-        var hiSeed = (uint)(seed >> 32);
+        var loSeed = (uint) (seed & 0xFFFFFFFF);
+        var hiSeed = (uint) (seed >> 32);
         key32[0] = BitOperations.RotateRight(BinaryPrimitives.ReverseEndianness(loSeed), 16);
         key32[1] = BitOperations.RotateLeft(BinaryPrimitives.ReverseEndianness(hiSeed), 8);
 
@@ -24,21 +24,21 @@ public static class Crypto {
         key[0] += 0x45;
         for (var i = 1; i < 8; ++i) {
             var tmp = key[i - 1] - 0x2C + key[i];
-            key[i] = (byte)(tmp ^ (key[i - 1] << 2) ^ 0x45);
+            key[i] = (byte) (tmp ^ (key[i - 1] << 2) ^ 0x45);
         }
 
         // expand key to all 256 bits
         var kidx = 2;
         for (var i = 0; i < 32; ++i) {
             var a = 5 * key64[(kidx >> 1) - 1];
-            a ^= (ulong)key32[kidx - 1] << 32;
-            key32[kidx] = (uint)(key32[kidx - 2] ^ a);
-            key32[kidx + 1] = (uint)(a >> 32);
+            a ^= (ulong) key32[kidx - 1] << 32;
+            key32[kidx] = (uint) (key32[kidx - 2] ^ a);
+            key32[kidx + 1] = (uint) (a >> 32);
             a = key32[kidx - 2] | (a & 0xFFFFFFFF00000000);
 
             var b = key32[kidx];
             kidx += 2;
-            key32[kidx - 2] = (uint)(a ^ b);
+            key32[kidx - 2] = (uint) (a ^ b);
             key32[kidx - 1] ^= key32[kidx - 3];
         }
 
@@ -53,7 +53,7 @@ public static class Crypto {
         var key64 = MemoryMarshal.Cast<byte, ulong>(key);
         for (var blockNo = 0u; blockNo < buffer.Length >> 3; ++blockNo) {
             key64[32] = blockNo << 23; // arbitrary but whatever.
-            var block = buffer.Slice((int)(blockNo << 3), 8);
+            var block = buffer.Slice((int) (blockNo << 3), 8);
             DecryptRound(key, block, blockNo);
             DecryptFinal(key, block, blockNo);
         }
@@ -64,7 +64,7 @@ public static class Crypto {
         var key64 = MemoryMarshal.Cast<byte, ulong>(key);
         for (var blockNo = 0u; blockNo < buffer.Length >> 3; ++blockNo) {
             key64[32] = blockNo << 23;
-            var block = buffer.Slice((int)(blockNo << 3), 8);
+            var block = buffer.Slice((int) (blockNo << 3), 8);
             EncryptFinal(key, block, blockNo);
             EncryptRound(key, block, blockNo);
         }
@@ -75,13 +75,13 @@ public static class Crypto {
         var key64 = MemoryMarshal.Cast<byte, ulong>(key);
         var buffer32 = MemoryMarshal.Cast<byte, uint>(buffer);
         var buffer64 = MemoryMarshal.Cast<byte, ulong>(buffer);
-        var index = (int)(block << 3);
+        var index = (int) (block << 3);
 
         // generate block key
-        var bkey = (key64[32] | ((uint)index << 10) | (uint)index | ((ulong) block << 32)) + 0xA1652347;
+        var bkey = (key64[32] | ((uint) index << 10) | (uint) index | ((ulong) block << 32)) + 0xA1652347;
 
         // get expanded key
-        var ekey = key64[(int)(block % 32)];
+        var ekey = key64[(int) (block % 32)];
 
         // xor magic
         buffer64[0] = ekey ^ bkey ^ (buffer64[0] - ekey);
@@ -95,16 +95,16 @@ public static class Crypto {
         var key64 = MemoryMarshal.Cast<byte, ulong>(key);
         var buffer32 = MemoryMarshal.Cast<byte, uint>(buffer);
         var buffer64 = MemoryMarshal.Cast<byte, ulong>(buffer);
-        var index = (int)(block << 3);
+        var index = (int) (block << 3);
 
         // swap blocks
         (buffer32[0], buffer32[1]) = (buffer32[1], buffer32[0]);
 
         // generate block key
-        var bkey = (key64[32] | ((uint)index << 10) | (uint)index | ((ulong) block << 32)) + 0xA1652347;
+        var bkey = (key64[32] | ((uint) index << 10) | (uint) index | ((ulong) block << 32)) + 0xA1652347;
 
         // get expanded key
-        var ekey = key64[(int)(block % 32)];
+        var ekey = key64[(int) (block % 32)];
 
         buffer64[0] = (ekey ^ bkey ^ buffer64[0]) + ekey;
     }
@@ -113,16 +113,16 @@ public static class Crypto {
     private static void DecryptRound(Span<byte> key, Span<byte> buffer, uint block) {
         var offset = 0;
         // block specific xor byte
-        var xor = (byte)(0x45 ^ block);
+        var xor = (byte) (0x45 ^ block);
         do {
             // xor current byte
-            var tmp = (byte)(xor ^ buffer[offset]);
+            var tmp = (byte) (xor ^ buffer[offset]);
             xor = buffer[offset];
             buffer[offset] = tmp;
 
             for (var i = 0; i < 8; ++i) {
                 // perform 8 rounds of shuffling
-                buffer[offset] = (byte)(0x78 + (buffer[offset] - key[(int)(((block << 3) + i) % 256)]));
+                buffer[offset] = (byte) (0x78 + (buffer[offset] - key[(int) (((block << 3) + i) % 256)]));
             }
 
             offset++;
@@ -133,11 +133,11 @@ public static class Crypto {
     private static void EncryptRound(Span<byte> key, Span<byte> buffer, uint block) {
         var offset = 0;
         // block specific xor byte
-        var xor = (byte)(0x45 ^ block);
+        var xor = (byte) (0x45 ^ block);
         do {
             for (var i = 0; i < 8; ++i) {
                 // perform 8 rounds of shuffling
-                buffer[offset] = (byte)(0x88 + (byte)(buffer[offset] + key[(int)(((block << 3) + i) % 256)]));
+                buffer[offset] = (byte) (0x88 + (byte) (buffer[offset] + key[(int) (((block << 3) + i) % 256)]));
             }
 
             // xor current byte
